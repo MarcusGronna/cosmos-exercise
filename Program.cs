6f79cb0bccf0bef255058125867576f5baa59f00
Marcus Grönnå <mgronna@gmail.com>
2026-05-20 11:35:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string databaseName = "myDatabase"; // Name of the database to create or use
+string containerName = "myContainer"; // Name of the container to create or use
+
+// Load environment variables from .env file
+DotEnv.Load();
+var envVars = DotEnv.Read();
+string cosmosDbAccountUrl = envVars["DOCUMENT_ENDPOINT"];
+string accountKey = envVars["ACCOUNT_KEY"];
